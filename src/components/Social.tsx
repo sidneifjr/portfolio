eb9cfa44f53861685035b8a6ae4820d8a988513f
Sidnei Farias <sidneijr.77@gmail.com>
2023-12-25 22:41:04 -0300

    a11y: apply title to social links

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -3,15 +3,19 @@ import { Github, Linkedin, Mail } from 'lucide-react'
 export const Social = () => {
   return (
     <div className="flex gap-4">
-      <a href="https://www.linkedin.com/in/sidnei-farias/" target="blank">
+      <a
+        href="https://www.linkedin.com/in/sidnei-farias/"
+        title="Linkedin"
+        target="blank"
+      >
         <Linkedin className="text-white" />
       </a>
 
-      <a href="https://github.com/sidneifjr" target="blank">
+      <a href="https://github.com/sidneifjr" title="Github" target="blank">
         <Github className="text-white" />
       </a>
 
-      <a href="mailto:sfarias.dev@gmail.com" target="blank">
+      <a href="mailto:sfarias.dev@gmail.com" title="Email" target="blank">
         <Mail className="text-white" />
       </a>
     </div>
