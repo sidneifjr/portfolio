7aeb4a56407d5149295c0fa3ac0ca45645270c24
Sidnei Farias <sidneijr.77@gmail.com>
2023-12-29 20:42:42 -0300

    refactor(anim): reduces unnecessary props repetition

--- src/components/MotionWrapper.tsx
+++ src/components/MotionWrapper.tsx
@@ -1,31 +1,20 @@
 import { type ReactNode } from 'react'
 
-import { m, LazyMotion, domAnimation } from 'framer-motion'
+import { LazyMotion, domAnimation, m } from 'framer-motion'
 
-interface MotionWrapper {
-  children: ReactNode
-  initial: any
-  animate: any
-  transition: any
-  exit: any
+type MotionWrapperProps = {
   className: string
+  children: ReactNode
 }
 
-export const MotionWrapper = ({
-  children,
-  initial,
-  animate,
-  transition,
-  exit,
-  className,
-}: MotionWrapper) => {
+export const MotionWrapper = ({ children, className }: MotionWrapperProps) => {
   return (
     <LazyMotion features={domAnimation}>
       <m.div
-        initial={initial}
-        animate={animate}
-        transition={transition}
-        exit={exit}
+        initial={{ y: '25%', opacity: 0 }}
+        animate={{ y: '0%', opacity: 1 }}
+        transition={{ duration: 0.75, ease: [0.33, 1, 0.68, 1] }}
+        exit={{ y: '-15%', opacity: 0 }}
         className={className}
       >
         {children}
