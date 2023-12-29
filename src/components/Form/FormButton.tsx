1e3594291621c6fef671be298029427900dde8c6
Sidnei Farias <sidneijr.77@gmail.com>
2023-12-29 20:52:08 -0300

    fix(Contact): corrects types

--- src/components/Form/FormButton.tsx
+++ src/components/Form/FormButton.tsx
@@ -6,14 +6,14 @@ interface FormButtonProps
   children: ReactNode
 }
 
-export const FormButton = (
-  props: FormButtonProps,
-  { children }: FormButtonProps
-) => {
+export const FormButton = ({
+  'data-cy': dataCy,
+  children,
+}: FormButtonProps) => {
   return (
     <button
       className="w-24 text-lg min-h-10 font-medium text-white/50 text-center border-2 border-white/50 rounded-md py-1 px-4 mx-auto flex justify-center items-center transition placeholder:text-white placeholder:opacity-50 hover:border-white hover:text-white focus:text-white"
-      {...props}
+      data-cy={dataCy}
     >
       {children}
     </button>
--- src/components/Form/FormRoot.tsx
+++ src/components/Form/FormRoot.tsx
@@ -6,11 +6,16 @@ type FormRootProps = {
   children: ReactNode
 }
 
-export const FormRoot = (props: FormRootProps, { children }: FormRootProps) => {
+export const FormRoot = ({
+  onSubmit,
+  'data-cy': dataCy,
+  children,
+}: FormRootProps) => {
   return (
     <form
       className="w-full max-w-[676px] m-auto flex flex-1 flex-col justify-center items-center gap-7"
-      {...props}
+      onSubmit={onSubmit}
+      data-cy={dataCy}
     >
       {children}
     </form>
