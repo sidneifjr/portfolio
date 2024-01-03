acca0636b4b4c5af52ec84757d54c6489d37fc2e
Sidnei Farias <sidneijr.77@gmail.com>
2024-01-03 13:46:01 -0300

    styles(Form): corrects submit button styling

--- src/components/Form/FormButton.tsx
+++ src/components/Form/FormButton.tsx
@@ -12,7 +12,7 @@ export const FormButton = ({
 }: FormButtonProps) => {
   return (
     <button
-      className="w-24 text-lg min-h-10 font-medium text-white/50 text-center border-2 border-white/50 rounded-md py-1 px-4 mx-auto flex justify-center items-center transition placeholder:text-white placeholder:opacity-50 hover:border-white hover:text-white focus:text-white"
+      className="min-w-24 text-lg min-h-10 font-semibold text-white text-center uppercase bg-blue rounded-md py-1 px-4 mx-auto flex justify-center items-center transition placeholder:text-white placeholder:opacity-50 hover:border-white hover:text-white focus:text-white"
       data-cy={dataCy}
     >
       {children}
